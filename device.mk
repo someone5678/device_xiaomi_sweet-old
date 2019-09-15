cbd0fe803258242a5ab818c88bc0c740dfb8d76d
PIPIPIG233666 <pig.priv@gmail.com>
2019-09-14 21:46:19 -0400

    sweet: audio: Rename audio_tuning_mixer_tavil.txt to audio_tuning_mixer.txt
     * Silence the following:
       ACDB-MCS: acdb_mcs_init: /vendor/etc/audio_tuning_mixer.txt not present, using /vendor/etc/audio_tuning_mixer.txt config file
       MCS-RT-CTL: Can't open the configuration file /vendor/etc/audio_tuning_mixer.txt.
    
    Change-Id: I959588613a61c57b417e93a38757281fb178f586

--- device.mk
+++ device.mk
@@ -20,7 +20,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/configs/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_idp.xml \
-    $(LOCAL_PATH)/configs/audio/audio_tuning_mixer_tavil.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer_tavil.txt
+    $(LOCAL_PATH)/configs/audio/audio_tuning_mixer_tavil.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt
 
 PRODUCT_PACKAGES += \
     libvolumelistener \
