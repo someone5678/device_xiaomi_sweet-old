9ce3be119768836c1bac6751c80409054e281143
Hernán Castañón Álvarez <herna@paranoidandroid.co>
2019-10-10 12:19:21 +0000

    sweet: add custom msm_irqbalance.conf
    Unbalancing msm_drm and kgsl-3d0 reduces display latency and overall jank.
    
    Signed-off-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>

--- device.mk
+++ device.mk
@@ -228,6 +228,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libqti-perfd-client
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service \
