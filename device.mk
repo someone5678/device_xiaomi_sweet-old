bf4f2e9f95e9f6c2cee3dbab108588b27ebfecd5
Danny Lin <danny@kdrag0n.dev>
2020-11-03 13:04:47 -0800

    apps: Build QuickAccessWallet plugin for SystemUI
    This plugin that provides the wallet (cards and passes) integration in
    the power menu is now open-source, so let's build it.
    
    Change-Id: I33b31d3f70b3f92c6b1d1114b26e0253283a49d6

--- device.mk
+++ device.mk
@@ -162,6 +162,10 @@ PRODUCT_PACKAGES += \
     TelephonyOverlaySweet \
     WifiOverlay
 
+# SystemUI plugins
+PRODUCT_PACKAGES += \
+    QuickAccessWallet
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.ims.xml \
