346ad50ffe26042816a927bf428d12f52303069d
etahamad <etahamad@icloud.com>
2021-03-19 08:59:18 +0200

    sweet: Introduce IORap
    Commit b73d5516136d1a4de39afd74f564daf70b946889 in build/make (rvc-qpr2)
    disabled IORap by default due to a "regression" without mentioning what
    the regression is. Considering how it's been working fine for the past 6
    months and has demonstrated improvements to app launch time [1], let's
    re-enable it here.
    
    [1] https://medium.com/androiddevelopers/improving-app-startup-with-i-o-prefetching-62fbdb9c9020
    
    Co-authored-by: raiyanbinmohsin <raiyanbinmohsinshishir@gmail.com>
    Co-authored-by: DillerOFire <niktofe1@gmail.com>
    Co-authored-by: Nikhil Baddi <nikhilbaddi98@gmail.com>
    Signed-off-by: pkm774 <mprabhat774@gmail.com>
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- device.mk
+++ device.mk
@@ -59,6 +59,10 @@ PRODUCT_PACKAGES += \
     fastbootd \
     android.hardware.fastboot@1.0-impl-mock
 
+# Iorap
+PRODUCT_PACKAGES += \
+    iorap-app-startup-runner
+
 # IPACM
 PRODUCT_PACKAGES += \
     IPACM_cfg.xml \
