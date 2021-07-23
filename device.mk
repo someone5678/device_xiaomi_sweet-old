7c43576365dfc011eabc45bc13d811f40381d0c3
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-23 16:53:20 +0600

    sweet: wifi: Import wifi configs
    * From sweet-user 11 RKQ1.200826.002 V12.5.4.0.RKFMIXM release-keys
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I5c9dd48927c5df4ae1630d6f1bcc0402df49c46a
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- device.mk
+++ device.mk
@@ -217,6 +217,8 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # WiFi Display
