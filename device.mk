5f5ce8bdb8260f0c31080242e4fb4226f211ee21
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-09-06 17:44:15 +0200

    sweet: Switch to OSS IPACM stack
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- device.mk
+++ device.mk
@@ -58,6 +58,12 @@ PRODUCT_PACKAGES += \
     fastbootd \
     android.hardware.fastboot@1.0-impl-mock
 
+# IPACM
+PRODUCT_PACKAGES += \
+    IPACM_cfg.xml \
+    ipacm \
+    ipacm.rc
+
 # Init
 PRODUCT_PACKAGES += \
     init.qcom.rc \
@@ -185,7 +191,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    vendor/qcom/opensource/data-ipa-cfg-mgr
 
 # Perf
 PRODUCT_PACKAGES += \
