36d0d8cd9d38642ce944869239b87b973862ccc0
someone5678 <nemui3353@gmail.com>
2021-11-22 04:28:48 +0900

    sweet: build libhidlbase
    * libhidlbase contains all of symbols previously in libhidltransport and libhwbinder since Android 10
    
    ref: https://source.android.com/devices/architecture/hidl-cpp/packages#link-libraries

--- device.mk
+++ device.mk
@@ -64,7 +64,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
-    libhwbinder.vendor
+    libhidlbase \
+    libhidlbase.vendor
 
 # Fastbootd
 PRODUCT_PACKAGES += \
