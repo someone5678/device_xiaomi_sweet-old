c97b8ad8652939fb7e4bcb77d87e72d44c7b2137
someone5678 <nemui3353@gmail.com>
2021-11-25 15:44:58 +0900

    sweet: build android.hardware.power-service from source
    * also import android.hardware.power-service.rc from vendor tree

--- device.mk
+++ device.mk
@@ -227,6 +227,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power-service \
+    android.hardware.power-service.rc \
     android.hardware.power-service.sweet-libperfmgr \
     android.hardware.power.stats@1.0-service.mock
 
