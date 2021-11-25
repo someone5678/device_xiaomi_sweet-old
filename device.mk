d320ddef2e4aeecf3f915f362cde9458bfd60f5e
someone5678 <nemui3353@gmail.com>
2021-11-25 15:42:15 +0900

    sweet: build libjson from source

--- device.mk
+++ device.mk
@@ -450,7 +450,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.6.vendor \
     android.hardware.radio.config@1.3.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.secure_element@1.2.vendor
+    android.hardware.secure_element@1.2.vendor \
+    libjson
 
 # Network
 PRODUCT_PACKAGES += \
