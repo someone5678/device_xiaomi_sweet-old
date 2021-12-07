2c6d3225b6d7625c3943ceea37dbdab4678aa57e
someone5678 <nemui3353@gmail.com>
2021-12-08 02:41:40 +0900

    sweet: patch: drop writable ccache patch
    * merged

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,10 +3,6 @@ rm -Rf device/qcom/sepolicy && git clone git@github.com:ArrowOS/android_device_q
 rm -Rf device/qcom/sepolicy_vndr && git clone git@github.com:ArrowOS/android_device_qcom_sepolicy_vndr.git --depth 1 device/qcom/sepolicy_vndr
 git apply --directory='device/qcom/sepolicy_vndr' device/xiaomi/sweet/.patch/device_qcom_sepolicy_vndr-adapt-to-PE.patch
 
-# Writable CCache ref: https://github.com/ArrowOS/android_build_soong/commit/4e66d214dddd63ef780a8a10c41e9e14ac61f9bc
-cd build/soong && git reset --hard && cd ../../
-git apply --directory='build/soong' device/xiaomi/sweet/.patch/build_soong-ccache-patch.patch
-
 # PixelPropsUtils Dialer whitelisting
 cd frameworks/base && git reset --hard && cd ../../
 git apply --directory='frameworks/base' device/xiaomi/sweet/.patch/frameworks_base-PixelPropsUtils.patch
