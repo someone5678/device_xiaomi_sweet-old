ee0abff2f2f649573b8df438b971f239abd47645
someone5678 <nemui3353@gmail.com>
2021-12-17 17:25:49 +0900

    sweet: update fp and desc to december raven release

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -24,7 +24,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SD1A.210817.036 7805805 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.211205.016.A1 7957957 release-keys"
 
-BUILD_FINGERPRINT := google/raven/raven:12/SD1A.210817.036/7805805:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.211205.016.A1/7957957:user/release-keys
 
