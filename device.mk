e771147386ca11b53c95876322e629a06e2309af
pkm774 <mprabhat774@gmail.com>
2021-12-29 21:29:18 +0100

    sweet: Build missing sensor packages
    12-17 10:31:16.039   559   559 I hwservicemanager: getTransport: Cannot find entry android.hardware.sensors@2.1::ISensors/default in either framework or device manifest.
    12-17 10:31:16.040   559   559 I hwservicemanager: getTransport: Cannot find entry android.hardware.sensors@2.0::ISensors/default in either framework or device manifest.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I4a5fbf1fd317a5cc50094d4c81ba49567e3278d5

--- device.mk
+++ device.mk
@@ -524,6 +524,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@2.0 \
+    android.hardware.sensors@2.1 \
     libsensorndkbridge
 
 # USB
