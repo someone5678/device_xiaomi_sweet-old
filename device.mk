de14819b609ac86c0ffbab4d2e1631c97dfdd7a7
someone5678 <nemui3353@gmail.com>
2022-02-22 23:54:16 +0900

    sweet: prebuilts: add permissions for CameraGo
    * also move CameraGo to product partition

--- device.mk
+++ device.mk
@@ -323,6 +323,7 @@ PRODUCT_PACKAGES += \
     libgui_vendor \
     libxml2 \
     CameraGo \
+    privapp-permissions-camera-go.xml \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_PACKAGES += \
