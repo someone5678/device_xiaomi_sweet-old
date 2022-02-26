dca2c5b836dbf02ca33a4c9cd1f7092d24863bee
someone5678 <nemui3353@gmail.com>
2022-02-26 23:24:40 +0900

    sweet: patch/vendorsetup: switch to PE qcom sepolicy repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,3 @@
-# Sepolicy
-rm -Rf device/qcom/sepolicy && git clone git@github.com:ArrowOS/android_device_qcom_sepolicy.git --depth 1 device/qcom/sepolicy
-rm -Rf device/qcom/sepolicy_vndr && git clone git@github.com:ArrowOS/android_device_qcom_sepolicy_vndr.git --depth 1 device/qcom/sepolicy_vndr
-git apply --directory='device/qcom/sepolicy_vndr' device/xiaomi/sweet/.patch/device_qcom_sepolicy_vndr-adapt-to-PE.patch
-
 # PixelPropsUtils Dialer whitelisting
 cd frameworks/base && git reset --hard && cd ../../
 git apply --directory='frameworks/base' device/xiaomi/sweet/.patch/frameworks_base-PixelPropsUtils.patch
