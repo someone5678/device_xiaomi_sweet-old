46fa4094e9b7221ccdcb4e35e1694ee296611e73
someone5678 <nemui3353@gmail.com>
2022-03-01 22:02:40 +0900

    sweet: drop patches and vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,6 +0,0 @@
-# PixelPropsUtils Dialer whitelisting
-cd frameworks/base && git reset --hard && cd ../../
-git apply --directory='frameworks/base' device/xiaomi/sweet/.patch/frameworks_base-PixelPropsUtils.patch
-
-# Custom hardware_qcom-caf_sm8150-common_display repo
-rm -Rf hardware/qcom-caf/sm8150-common/display && git clone git@github.com:someone5678/hardware_qcom-caf_sm8150-common_display.git --depth 1 hardware/qcom-caf/sm8150-common/display
