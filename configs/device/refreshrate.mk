a868cdd34ad3112528a934472ab1d91f17ac5685
Andrzej Perczak <linux@andrzejperczak.com>
2022-03-05 16:58:25 +0530

    sm6150-common: Update surfaceflinger props
    This change is based on sm8350 configuration in display hal [1].
    Additionally, I removed old nonexistent props.
    
    Please note that debug.sf.disable_backpressure is present in R (however removed in S) but
    is not enabled on google devices nor newest qcom SOCs.
    
    This change made UI much more fluent.
    
    [1] https://source.codeaurora.org/quic/la/platform/hardware/qcom/display/tree/config/display-product.mk?h=LA.UM.9.16.r2-02800-MANNAR.0
    
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: TejasKamat <kamattejas23@gmail.com>
    
    ref:
    https://github.com/BulletFlux/device_xiaomi_sm6150-common/commit/d002553a6d9a08d77722142bd9ac725a0685fafa
    
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- configs/device/refreshrate.mk
+++ configs/device/refreshrate.mk
@@ -7,12 +7,12 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_idle_timer_ms?=500 \
     ro.surface_flinger.set_touch_timer_ms=750 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
-    ro.surface_flinger.use_content_detection_for_refresh_rate=true \
-    ro.surface_flinger.protected_contents=true
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=0 \
     debug.sf.enable_gl_backpressure=1 \
+    debug.sf.disable_client_composition_cache=1 \
     vendor.display.idle_time=0 \
     vendor.display.idle_time_inactive=0
 
@@ -43,8 +43,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_touch_timer_ms=750 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.support_kernel_idle_timer=true \
-    ro.surface_flinger.use_content_detection_for_refresh_rate=true \
-    ro.surface_flinger.protected_contents=true
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
 # Enable app/sf phase offset as durations. The numbers below are translated from the existing
 # positive offsets by finding the duration app/sf will have with the offsets.
@@ -66,6 +65,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=0 \
     debug.sf.enable_gl_backpressure=1 \
+    debug.sf.disable_client_composition_cache=1 \
     vendor.display.disable_metadata_dynamic_fps=1 \
     vendor.display.idle_time=0 \
     vendor.display.idle_time_inactive=0
