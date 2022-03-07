6c6180fa7d326ad16903d198caba5520742dd479
Arian <arian.kulmer@web.de>
2022-03-07 14:00:40 +0100

    sweet: gps: Build GPS HAL from source
    slim_daemon is disabled in izat.conf, hence drop it.
    
    Change-Id: Ia1ed33f6250b898d149970389befe8033cc8eaf3

--- device.mk
+++ device.mk
@@ -549,30 +549,46 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libvulkan
 
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.1 \
+    android.hardware.gnss.measurement_corrections@1.1 \
+    android.hardware.gnss.measurement_corrections@1.1.vendor \
+    android.hardware.gnss.visibility_control@1.0 \
+    android.hardware.gnss.visibility_control@1.0.vendor
+
 # GPS
 PRODUCT_PACKAGES += \
-     android.hardware.gnss@2.1-impl-qti \
-     android.hardware.gnss@2.1-service-qti \
-     android.hardware.gnss@2.1 \
-     android.hardware.gnss@2.1.vendor \
-     android.hardware.gnss.measurement_corrections@1.1 \
-     android.hardware.gnss.measurement_corrections@1.1.vendor \
-     android.hardware.gnss.visibility_control@1.0 \
-     android.hardware.gnss.visibility_control@1.0.vendor
+    android.hardware.gnss@2.1.vendor \
+    android.hardware.gnss@2.1-impl-qti \
+    android.hardware.gnss@2.1-service-qti
 
 PRODUCT_PACKAGES += \
-     gps.conf \
-     flp.conf \
-     gnss_antenna_info.conf \
-     libloc_pla_headers \
-     liblocation_api_headers \
-     libgps.utils_headers \
-     liblocation_api \
-     libgps.utils \
-     libbatching \
-     libgeofencing \
-     libloc_core \
-     libgnss
+    libbatching \
+    libgeofencing \
+    liblocation_api \
+    liblocation_api_headers \
+    libloc_core \
+    libloc_pla_headers \
+    libgps.utils \
+    libgps.utils_headers \
+    libgnss
+
+PRODUCT_PACKAGES += \
+    apdr.conf \
+    flp.conf \
+    gnss_antenna_info.conf \
+    gps.conf \
+    izat.conf \
+    lowi.conf \
+    sap.conf \
+    xtwifi.conf
+
+PRODUCT_PACKAGES += \
+    gnss@2.0-base.policy \
+    gnss@2.0-xtra-daemon.policy \
+    gnss@2.0-xtwifi-client.policy \
+    gnss@2.0-xtwifi-inet-agent.policy
 
 # Fingerprint
 PRODUCT_PACKAGES += \
