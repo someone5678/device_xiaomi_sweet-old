4dfc0f8ef57230420dd63119e7e2192523321be7
someone5678 <nemui3353@gmail.com>
2022-06-18 02:14:06 +0900

    sweet: switch to customized CrDroid Android sm6150 kernel source
    * Fork from:
    https://github.com/crdroidandroid/android_kernel_xiaomi_sm6150

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,7 +67,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := sweet_defconfig
+TARGET_KERNEL_CONFIG := sweet_user_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sweet
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_VERSION := 4.14
