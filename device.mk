cd11a2d09bab86567037b7cf8eb62aeaa17421b6
someone5678 <nemui3353@gmail.com>
2022-09-04 20:56:18 +0900

    sweet: Downrev android.hardware.bluetooth from 1.1 to 1.0
    09-04 20:37:39.513   603   603 I hwservicemanager: Since android.hardware.bluetooth@1.1::IBluetoothHci/default is not registered, trying to start it as a lazy HAL.
    
    * Since we switched from QTI BT Stack to AOSP one, we should downrev it.
      Otherwise, Bluetooth will not work.
    
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- device.mk
+++ device.mk
@@ -574,7 +574,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.1.vendor \
+    android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     libbtconfigstore \
