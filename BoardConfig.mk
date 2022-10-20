9ec54a41a07689efdbac885b4191adeb7c07f8b5
someone5678 <nemui3353@gmail.com>
2022-10-21 02:46:08 +0900

    sweet: Qcom-common: Update FCM path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,7 +151,7 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/vendor_framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
